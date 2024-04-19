96204c24a93b82d2d134482296ce707a6f230afd
iftikarjahan <69801895+iftikarjahan@users.noreply.github.com>
2024-04-19 12:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
 let url=window.location.href;
-let len=url.trim().length;
-alert(`The length of the URL is: ${len} `);
+let len=url.length;
+alert(`The length of the URL is: ${len}`)
+// The length of the URL is:22 
